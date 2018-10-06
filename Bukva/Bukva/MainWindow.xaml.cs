2d7186fe05ad4617708ffb5cf33d079255037422
vzyrianov <vzyriano@kent.edu>
2018-10-06 00:01:59 -0400

    Remove comment

--- Bukva/Bukva/MainWindow.xaml.cs
+++ Bukva/Bukva/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace Bukva
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
