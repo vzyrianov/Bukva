4cf38a011d95399257f2b094b3ec69424ea77410
vzyrianov <vzyriano@kent.edu>
2018-10-05 23:29:31 -0400

    Move Win32 function signatures to Win32 class

--- Bukva/Bukva/KeyboardHook.cs
+++ Bukva/Bukva/KeyboardHook.cs
@@ -8,26 +8,7 @@ namespace Bukva
 {
     class KeyboardHook : IDisposable
     {
-        private const int WH_KEYBOARD_LL = 13;
-        private const int WM_KEYDOWN = 0x0100;
-        private const int WM_SYSKEYDOWN = 0x0104;
-
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr SetWindowsHookEx(int idHook, CallBackHandler lpfn, IntPtr hMod, uint dwThreadId);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-        
-        public delegate IntPtr CallBackHandler(int nCode, IntPtr wParam, IntPtr lParam);
-
-        private CallBackHandler proc;
+        private Win32.CallBackHandler proc;
         private IntPtr hookID = IntPtr.Zero;
 
         public event EventHandler<KeyPressedEventArgs> OnKeyPressed;
@@ -46,18 +27,18 @@ namespace Bukva
             using (Process curProcess = Process.GetCurrentProcess())
             using (ProcessModule curModule = curProcess.MainModule)
             {
-                hookID = SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+                hookID = Win32.SetWindowsHookEx(Win32.WH_KEYBOARD_LL, proc, Win32.GetModuleHandle(curModule.ModuleName), 0);
             }
         }
         
         public void Dispose()
         {
-            UnhookWindowsHookEx(hookID);
+            Win32.UnhookWindowsHookEx(hookID);
         }
 
         private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
-            if (nCode >= 0 && (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYDOWN))
+            if (nCode >= 0 && (wParam == (IntPtr)Win32.WM_KEYDOWN || wParam == (IntPtr)Win32.WM_SYSKEYDOWN))
             {
                 int virtualKeyCode = Marshal.ReadInt32(lParam);
                 Key pressedKey = KeyInterop.KeyFromVirtualKey(virtualKeyCode);
@@ -72,7 +53,7 @@ namespace Bukva
                 }
             }
 
-            return CallNextHookEx(hookID, nCode, wParam, lParam);
+            return Win32.CallNextHookEx(hookID, nCode, wParam, lParam);
         }
 
         public void TrapKey(Key k)
---
+++ Bukva/Bukva/Win32.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+
+namespace Bukva
+{
+    public class Win32
+    {
+        public const int WH_KEYBOARD_LL = 13;
+        public const int WM_KEYDOWN = 0x0100;
+        public const int WM_SYSKEYDOWN = 0x0104;
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr SetWindowsHookEx(int idHook, CallBackHandler lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public  static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+
+        public delegate IntPtr CallBackHandler(int nCode, IntPtr wParam, IntPtr lParam);
+    }
+}
