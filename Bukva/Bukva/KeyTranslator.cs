7a81b62daa57a34aef52bf68245840e9b8fc715f
vzyrianov <vzyriano@kent.edu>
2018-10-20 20:27:09 -0400

    Create class for performing translations

---
+++ Bukva/Bukva/KeyTranslator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bukva
+{
+    class KeyTranslator
+    {
+        public bool Enabled { get; set; }
+
+        KeyboardHook keyboardHook;
+        
+        public KeyTranslator()
+        {
+            keyboardHook = new KeyboardHook();
+            keyboardHook.OnKeyPressed += KeyboardHook_OnKeyPressed; ;
+        }
+
+        private void KeyboardHook_OnKeyPressed(object sender, KeyPressedEventArgs e)
+        {
+
+        }
+    }
+}
