200ad043a9e3e032fe3d81c8fcc856b60d6f005c
Vlas Zyrianov <vzyriano@kent.edu>
2019-04-04 16:51:24 -0400

    Add read in method

--- Bukva/Bukva/LetterTable.cs
+++ Bukva/Bukva/LetterTable.cs
@@ -1,13 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
 
 namespace Bukva
 {
-    class LetterTable
+    class LetterTable : Dictionary<string, string>
     {
+        public LetterTable() : base()
+        {
 
+        }
+
+        void ReadInFromFile()
+        {
+            string line;
+
+            using (StreamReader streamReader = new StreamReader("lang.buk"))
+            {
+                line = streamReader.ReadLine();
+                var tokens = line.Split(',');
+                Add(tokens[0], tokens[1]);
+            }
+        }
     }
 }
